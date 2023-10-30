d6bc8867e746406ae1d65beb2b02e930f4716715
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-10-29 21:23:18 -0500

    feat | handle blog creation error

--- src/modules/blog.rs
+++ src/modules/blog.rs
@@ -5,4 +5,4 @@ mod parse;
 pub use db::{by_id, create, get_all};
 pub use models::{Blog, BlogById, BlogPreview};
 
-pub use parse::{parse, BlogParse};
+pub use parse::{parse, BlogParse, Error as ParseError};
--- src/routes/blogs/create_one.rs
+++ src/routes/blogs/create_one.rs
@@ -1,9 +1,12 @@
-use actix_web::{post, web::Data, Responder};
+use actix_web::{post, web::Data, HttpResponse, Responder, ResponseError};
 
 use crate::{
-    modules::{admin::AdminId, blog::{self, BlogParse}, db::Pool},
+    modules::{
+        admin::AdminId,
+        blog::{self, BlogParse},
+        db::Pool,
+    },
     shared::extractors::valid_json::ValidJson,
-    sqlx::void_insert_response,
 };
 
 use serde::Deserialize;
@@ -15,19 +18,53 @@ pub struct Request {
     pub content: String,
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    Parse(#[from] blog::ParseError),
+    #[error("Conflict creating blog")]
+    Conflict,
+    #[error("Database error")]
+    Database,
+}
+
+impl ResponseError for Error {
+    fn status_code(&self) -> actix_web::http::StatusCode {
+        match self {
+            Self::Parse(_) => actix_web::http::StatusCode::BAD_REQUEST,
+            Self::Conflict => actix_web::http::StatusCode::CONFLICT,
+            Self::Database => actix_web::http::StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+impl From<sqlx::Error> for Error {
+    fn from(e: sqlx::Error) -> Self {
+        match e {
+            sqlx::Error::Database(_) => Self::Conflict,
+            _ => Self::Database,
+        }
+    }
+}
+
 #[post("/")]
 pub async fn endpoint(
     pool: Data<Pool>,
     req: ValidJson<Request>,
     AdminId { id }: AdminId,
-) -> impl Responder {
+) -> Result<impl Responder, Error> {
     let Request { content } = req.as_ref();
 
     let BlogParse {
         title,
         content: html_content,
-    } = blog::parse(content).expect("Foo");
+    } = blog::parse(content)?;
 
-    let result = blog::create(pool.get_ref(), id, &title, content, &html_content).await;
-    void_insert_response(result)
+    let result = blog::create(pool.get_ref(), id, &title, content, &html_content).await?;
+
+    if result.rows_affected() == 0 {
+        return Err(Error::Conflict);
+    }
+
+    Ok(HttpResponse::Created().finish())
 }
