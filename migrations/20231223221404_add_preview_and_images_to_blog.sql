fd3ea8f2b0f208ef17c31bf539cb1192eef63928
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2023-12-23 17:50:02 -0500

    feat(db): add main_image to blog table

--- migrations/20231223221404_add_preview_and_images_to_blog.sql
+++ migrations/20231223221404_add_preview_and_images_to_blog.sql
@@ -1,3 +1,4 @@
 -- Add migration script here
-ALTER TABLE blogs ADD COLUMN preview TEXT;
-ALTER TABLE blogs ADD COLUMN images TEXT[];
+ALTER TABLE blogs ADD COLUMN preview TEXT NOT NULL DEFAULT '';
+ALTER TABLE blogs ADD COLUMN images TEXT[] NOT NULL DEFAULT '{}';
+ALTER TABLE blogs ADD COLUMN main_image TEXT;
