ab134e327159dd0ac9ef64e1435005d0bce5d74e
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-01-07 10:45:34 -0500

    refactor(user): ConvertToAdminId gets build correclty

--- src/domain/user/features.rs
+++ src/domain/user/features.rs
@@ -1,4 +1,5 @@
 pub mod convert_to_admin_id {
+    use actix_web::web::Data;
     use sqlx::query;
     use uuid::Uuid;
 
@@ -6,11 +7,14 @@ pub mod convert_to_admin_id {
         domain::user::value_objects::AdminId, persistence::db::Pool, server::service::sync_service,
     };
 
-    sync_service!(ConvertToAdminId; pool: Pool);
+    sync_service!(ConvertToAdminId; pool: Data<Pool>);
 
     impl ConvertToAdminId {
         pub async fn run(&self, id: Uuid) -> Result<AdminId, sqlx::Error> {
-            let _ = query!("SELECT id FROM admins WHERE id = $1", id).fetch_one(self.pool.as_ref()).await?;
+            let _ = query!("SELECT id FROM admins WHERE id = $1", id)
+                .fetch_one(self.pool.as_ref())
+                .await?;
+
             Ok(AdminId::unchecked_new(id))
         }
     }
