957fda3d403923b5a93e9a189d43c2c55e9074ec
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-05-08 13:52:40 -0500

    feat(markdown-parse): create crs package

--- libs/markdown-islands/src/lib.rs
+++ libs/markdown-islands/src/lib.rs
@@ -3,8 +3,9 @@ use leptos::{island, view, Children, IntoView};
 #[island]
 pub fn CodeBlock(language: Option<String>, children: Children) -> impl IntoView {
     view! {
-        <pre><code>{children()}</code>
-        <!-- "generated!" -->
+        <pre>
+            <code>{children()}</code>
+            <span>{language}</span>
         </pre>
     }
 }
