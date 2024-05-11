fb9edc116978e919489a43089c33342b78dffd8c
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-05-11 10:40:19 -0500

    refactor(markdown-islands): make web-sys only avaliable in hydration

--- libs/markdown-islands/src/unstable.rs
+++ libs/markdown-islands/src/unstable.rs
@@ -1,10 +1,13 @@
 use leptos::window;
 
 pub fn copy_to_clipboard(text: &str) {
-    let Some(clipboard) = window().navigator().clipboard() else {
-        leptos::logging::warn!("No clipboard available");
-        return;
-    };
+    #[cfg(feature = "hydrate")]
+    {
+        let Some(clipboard) = window().navigator().clipboard() else {
+            leptos::logging::warn!("No clipboard available");
+            return;
+        };
 
-    let _ = clipboard.write_text(&text);
+        let _ = clipboard.write_text(&text);
+    }
 }
