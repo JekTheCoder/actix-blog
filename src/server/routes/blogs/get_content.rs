4a98d538e45f719060cd4926b1a4d7658ba04c22
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-05-16 21:24:53 -0500

    feat(blog): change get content endpoint to content.md

--- src/server/routes/blogs/get_content.rs
+++ src/server/routes/blogs/get_content.rs
@@ -3,7 +3,7 @@ use uuid::Uuid;
 
 use crate::{domain::blog::features::get_content::GetContent, server::admin::IsAdminFactory};
 
-#[get("/{blog_id}/content.md", wrap = "IsAdminFactory")]
+#[get("/{blog_id}/content.md/", wrap = "IsAdminFactory")]
 pub async fn endpoint(get_by_id: GetContent, id: Path<Uuid>) -> HttpResponse {
     match get_by_id.run(id.into_inner()).await {
         Ok(Some(blog)) => HttpResponse::Ok().body(blog),
