6e8c5591cdad6b7f31698d506691f83f7a205697
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-06-06 21:51:32 -0500

    refactor: remove warnings from copy clipboard module

--- libs/markdown-islands/src/unstable.rs
+++ libs/markdown-islands/src/unstable.rs
@@ -1,13 +1,18 @@
-use leptos::window;
-
 pub fn copy_to_clipboard(text: &str) {
     #[cfg(feature = "hydrate")]
     {
+        use leptos::window;
+
         let Some(clipboard) = window().navigator().clipboard() else {
             leptos::logging::warn!("No clipboard available");
             return;
         };
 
-        let _ = clipboard.write_text(&text);
+        let _ = clipboard.write_text(text);
+    }
+
+    #[cfg(not(feature = "hydrate"))]
+    {
+        let _ = text;
     }
 }
